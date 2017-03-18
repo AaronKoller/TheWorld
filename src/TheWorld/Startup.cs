6189494ddac9b22142ec0a956cda4f3363b65673
Aaron Koller <aaronkoller777@gmail.com>
2017-03-18 14:33:58 -0700

    Added an unirdered list.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -24,9 +19,7 @@ namespace TheWorld
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
