8cd5e466813998dfcf38caa56eb084b63c59bf71
Aaron Koller <aaronkoller777@gmail.com>
2017-03-24 23:19:52 -0700

    Validation has been added to the contact page.

--- src/TheWorld/ViewModels/ContactViewModel.cs
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -4,13 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace TheWorld.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(4096, MinimumLength = 10)]
         public string Message { get; set; }
     }
 }
