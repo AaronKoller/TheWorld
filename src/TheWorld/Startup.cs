9fec84379e2788208ae179515d91ca7d672fc508
Aaron Koller <aaronkoller777@gmail.com>
2017-03-25 23:40:15 -0700

    Found a way to include the config.json to the solution.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TheWorld.Services;
@@ -9,11 +11,18 @@ namespace TheWorld
     public class Startup
     {
         private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
 
         public Startup(IHostingEnvironment env)
         {
             _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("config.json");
+
+            _config = builder.Build();
         }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
