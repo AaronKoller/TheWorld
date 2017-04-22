cebd36cea1ce537d437d6ea0df7b8341cc240a41
Aaron Koller <aaronkoller777@gmail.com>
2017-04-22 09:55:36 -0700

    Added bing maps to pull the GeoCoordinates of an address.

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -48,6 +48,8 @@ namespace TheWorld
 
             services.AddScoped<IWorldRepository, WorldRepository>();
 
+            services.AddTransient<GeoCoordsService>();
+
             services.AddTransient<WorldContextSeedData>();
 
             services.AddLogging();
